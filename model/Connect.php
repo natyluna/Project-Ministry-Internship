42a2d46323cd8e84700781505411dabd8b9ff728
JuanMa <Balocco753@gmail.com>
2021-06-02 12:57:54 -0300

    Aqui probé que ande el git

---
+++ model/Connect.php
@@ -0,0 +1,16 @@
+<?php
+
+    $mysqli = new mysqli("localhost", "usuario", "contraseña", "basedatos");
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+    echo $mysqli->host_info . "\n";
+
+    $mysqli = new mysqli("127.0.0.1", "usuario", "contraseña", "basedatos", 3306);
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    echo $mysqli->host_info . "\n";
+
+?>
