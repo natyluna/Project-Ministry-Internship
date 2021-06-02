eeded5867b3f48f4d01ac9371c1143e4af7ca34b
JuanMa <Balocco753@gmail.com>
2021-06-02 13:55:46 -0300

    clase de coneccion base

--- model/Connect.php
+++ model/Connect.php
@@ -1,21 +1,10 @@
 <?php
 
-    //MODELO DE APERTURA BBDD
-
-    $mysqli = new mysqli("localhost", "usuario", "contraseña", "basedatos");
-    if ($mysqli->connect_errno) {
-        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    class conection {
+        public static conexion(){
+            $mysqli = new mysqli("localhost", "usuario", "contraseña", "basedatos");
+            return $mysqli;
+        }
     }
-    echo $mysqli->host_info . "\n";
-
-    $mysqli = new mysqli("127.0.0.1", "usuario", "contraseña", "basedatos", 3306);
-    if ($mysqli->connect_errno) {
-        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-
-    echo $mysqli->host_info . "\n";
-
-    //OTRO CULIAO QUE NO ME SALEEEEE LPTM
-
 
 ?>
