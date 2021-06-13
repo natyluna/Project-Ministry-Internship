f779be4485537eb6db17be3fcda2e3633ce60264
Dev-Gix <jonycas998@gmail.com>
2021-06-13 17:46:52 -0300

    Testeo

--- views/registroCivil/index.php
+++ views/registroCivil/index.php
@@ -1,15 +1,11 @@
 <div class="d-flex align-items-center p-3 my-3 text-white rounded shadow-sm color-bluecat">
-
     <div class="col d-flex align-items-center">
-
         <img class="me-3" src="<?php echo img ?>princicon.png" alt="" width="48" height="48">
         <div class="lh-1">
             <h1 class="h5 mb-0 text-white lh-1 animate__animated animate__headShake">Ministerios y Tramites</h1>
             <h6 class="animate__animated animate__headShake">Gobierno de Catamarca</h6>
         </div>
-
     </div>
-
 </div>
 
 <div id="register">
@@ -17,17 +13,14 @@
         <main>
             <div class="py-5 text-center">
                 <h2>Solicitud de Partidas</h2>
-                <p class="lead">Completa el siguiente formulario con tus datos</p>
+                <p>Completa el siguiente formulario con tus datos</p>
             </div>
-
-
             <div id="register-row" class="row justify-content-center align-items-baseline col-sm-10 col-md-8 text-light">
-                <form action="" method="post" class="needs-validation pt-5" novalidate>
-
+                <form action="" method="POST" class="needs-validation pt-5" >
                     <div class="row g-3 ">
                         <div class="col-sm-6">
                             <label for="nombre" name="nombre" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" placeholder="" value="" required>
+                            <input type="text" class="form-control" name="nombre" placeholder="ej:Juan" required>
                             <div class="invalid-feedback">
                                 Campo obligatorio
                             </div>
@@ -35,7 +28,7 @@
 
                         <div class="col-sm-6">
                             <label for="apellido" class="form-label">Apellido</label>
-                            <input type="text" class="form-control" id="apellido" placeholder="" value="" required>
+                            <input type="text" class="form-control" name="apellido" placeholder="ej:Álvarez" required>
                             <div class="invalid-feedback">
                                 Campo obligatorio
                             </div>
@@ -44,7 +37,7 @@
                         <div class="col-12">
                             <label for="username" class="form-label">DNI</label>
                             <div class="input-group has-validation">
-                                <input type="text" class="form-control" id="username" placeholder="Ingresa tu DNI" required>
+                                <input type="number" class="form-control" name="username" placeholder="Ingresa tu DNI" required>
                                 <div class="invalid-feedback">
                                     Campo obligatorio
                                 </div>
@@ -53,55 +46,50 @@
 
                         <div class="col-12">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="tu@ejemplo.com" required>
+                            <input type="email" class="form-control" name="email" placeholder="tu@ejemplo.com" required>
                             <div class="invalid-feedback">
                                 Ingrese un email valido
                             </div>
                         </div>
 
-
-
-
                         <hr class="my-4">
 
                         <h4 class="mb-3 mt-0">Tipo de Partida</h4>
-
-                        <div class="my-2 ">
+                        <div class="partidas">
                             <div class="form-check">
-                                <input id="nacimiento" name="partida" type="radio" class="form-check-input" checked required>
+                                <input name="partida" type="radio" class="form-check-input " value="nacimiento" required>
                                 <label class="form-check-label" for="nacimiento">Nacimiento</label>
                             </div>
                             <div class="form-check">
-                                <input id="matrimonio" name="partida" type="radio" class="form-check-input" required>
+                                <input name="partida" type="radio" class="form-check-input" value="matrimonio" required>
                                 <label class="form-check-label" for="matrimonio">Matrimonio</label>
                             </div>
                             <div class="form-check">
-                                <input id="defuncion" name="partida" type="radio" class="form-check-input" required>
-                                <label class="form-check-label" for="defuncion">Defuncion</label>
+                                <input name="partida" type="radio" class="form-check-input" value="defuncion" required>
+                                <label class="form-check-label" for="defuncion">Defunción</label>
                             </div>
                         </div>
-
-                        <div id="nacimiento" class="hide">
+                        <div id="nacimiento" class="hide solicitud-Partida">
                             <div class="col-sm-6">
                                 <label for="nombre" name="fecha" class="form-label">Fecha Nacimiento</label>
-                                <input type="date" class="form-control" id="fecha" placeholder="" value="" required>
+                                <input type="date" class="form-control" id="fecha" required>
                                 <div class="invalid-feedback">
                                     Campo obligatorio
                                 </div>
                             </div>
 
                         </div>
-                        <div id="matrimonio" class="hide">
+                        <div id="matrimonio" class="hide solicitud-Partida">
                             <div class="col-sm-6">
                                 <label for="matrimonio" name="libreta" class="form-label">N° de Libreta</label>
-                                <input type="text" class="form-control" id="nombre" placeholder="" value="" maxlength="8" required >
+                                <input type="text" class="form-control" id="nombre" maxlength="8" required>
                                 <div class="invalid-feedback">
                                     Campo obligatorio
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <label for="matrimonio" name="acta" class="form-label">N° de Acta</label>
-                                <input type="text" class="form-control" id="nombre" placeholder="" value="" maxlength="6" required>
+                                <input type="text" class="form-control" id="nombre" maxlength="6" required>
                                 <div class="invalid-feedback">
                                     Campo obligatorio
                                 </div>
@@ -109,10 +97,10 @@
 
                         </div>
 
-                        <div id="defuncion" class="hide">
+                        <div id="defuncion" class="hide solicitud-Partida">
                             <div class="col-sm-6">
-                                <label for="nombre" name="fechadef" class="form-label">Fecha de Defuncion</label>
-                                <input type="date" class="form-control" id="nombre" placeholder="" value="" required>
+                                <label for="nombre" name="fechadef" class="form-label">Fecha de Defunción</label>
+                                <input type="date" class="form-control" id="nombre" required>
                                 <div class="invalid-feedback">
                                     Campo obligatorio
                                 </div>
@@ -121,8 +109,9 @@
                         </div>
                         <hr class="mt-3 ">
 
-                        <button class="w-100 btn btn-info btn-lg " type="submit">Solicitar</button>
+                        <input class="w-100 btn btn-info btn-lg " type="submit" value="Solicitar"></input>
                 </form>
             </div>
     </div>
 </div>
+<script src="assets/js/main.js" ;
