e2ceec94431be89ba8fbf69c7d3e807efa0a8414
JuanMa <Balocco753@gmail.com>
2021-06-17 23:37:28 -0300

    Busqueda Finalizada

--- models/PersonaModel.php
+++ models/PersonaModel.php
@@ -1,12 +1,22 @@
 <?php
 
-
-
 function MostrarPersonas($cone)
 {
     
     try {
         $query = $cone->prepare("SELECT * FROM personas limit 10000");
+        $query->execute();
+        $datos = $query->fetchAll();
+        return $datos;
+    } catch (PDOException $e) {
+        alerta("ERROR AL MOSTRAR LOS DATOS: $e");
+    }
+}
+
+function BuscarPersonas($cone,$busk)
+{
+    try {
+        $query = $cone->prepare("SELECT * FROM personas WHERE nombre LIKE '%$busk%' OR apellido LIKE '%$busk%' OR dni LIKE '%$busk%'");
         $query->execute();
         $datos = $query->fetchAll();
         return $datos;
